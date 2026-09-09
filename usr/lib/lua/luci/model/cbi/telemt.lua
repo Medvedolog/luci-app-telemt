45f1cf9045f5ee21ac6ff4b44ff8e8ad813dd2be
medvedolog <tenkayyy8@gmail.com>
2026-09-09 17:51:45 +0300

    luci: wire Direct relay memory budget into General Settings

--- usr/lib/lua/luci/model/cbi/telemt.lua
+++ usr/lib/lua/luci/model/cbi/telemt.lua
@@ -1,3 +1,5 @@
 local legacy = require "luci.model.cbi.telemt_legacy"
+local memory_budget = require "luci.model.cbi.telemt_memory_budget"
 local web_users = require "luci.model.cbi.telemt_users_web"
+legacy = memory_budget.attach(legacy)
 return web_users.attach(legacy)
