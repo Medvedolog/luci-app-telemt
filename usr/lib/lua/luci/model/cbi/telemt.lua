a880f9871aea0ebaf3a4288793993ae19dcf71b4
medvedolog <tenkayyy8@gmail.com>
2026-09-18 08:36:06 +0300

    luci: preserve CBI environment when loading model fragments

--- usr/lib/lua/luci/model/cbi/telemt.lua
+++ usr/lib/lua/luci/model/cbi/telemt.lua
@@ -1,5 +1,28 @@
-local legacy = require "luci.model.cbi.telemt_legacy"
-local memory_budget = require "luci.model.cbi.telemt_memory_budget"
-local web_users = require "luci.model.cbi.telemt_users_web"
+-- LuCI CBI injects Map/Value/etc. into the environment of the model file.
+-- Loading CBI fragments with require() loses that environment under ucodebridge
+-- (notably OpenWrt 24.10), so execute every fragment in this model's CBI env.
+
+local cbi_env = _ENV
+if setfenv and getfenv then
+    cbi_env = getfenv(1)
+end
+
+local function load_cbi_fragment(path)
+    local chunk, err
+    if setfenv then
+        chunk, err = loadfile(path)
+        if not chunk then error(err) end
+        setfenv(chunk, cbi_env)
+    else
+        chunk, err = loadfile(path, "t", cbi_env)
+        if not chunk then error(err) end
+    end
+    return chunk()
+end
+
+local legacy = load_cbi_fragment("/usr/lib/lua/luci/model/cbi/telemt_legacy.lua")
+local memory_budget = load_cbi_fragment("/usr/lib/lua/luci/model/cbi/telemt_memory_budget.lua")
+local web_users = load_cbi_fragment("/usr/lib/lua/luci/model/cbi/telemt_users_web.lua")
+
 legacy = memory_budget.attach(legacy)
 return web_users.attach(legacy)
